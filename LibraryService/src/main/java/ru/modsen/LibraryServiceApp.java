6d9d090d5e8d425e95c77caabcab6f5bcf6aeb2a
Best Juare <burdance2004@gmail.com>
2024-10-20 23:47:15 +0300

    main structure

---
+++ LibraryService/src/main/java/ru/modsen/LibraryServiceApp.java
@@ -0,0 +1,17 @@
+package ru.modsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class LibraryServiceApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(LibraryServiceApp.class,args);
+    }
+}
---
+++ StorageService/src/main/java/ru/modsen/StorageServiceApp.java
@@ -0,0 +1,14 @@
+package ru.modsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StorageServiceApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(StorageServiceApp.class,args);
+    }
+}
